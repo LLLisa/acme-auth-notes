10e9caae682ce00c6ec38add3bd8aeeb58060e6f
Lisa Knox <lisaknox83@gmail.com>
2022-04-10 13:19:16 -0400

    notes modeled and seeded

--- db.js
+++ db.js
@@ -3,84 +3,108 @@ const bcrypt = require('bcrypt');
 const Sequelize = require('sequelize');
 const { STRING } = Sequelize;
 const config = {
-  logging: false
+  logging: false,
 };
 
-if(process.env.LOGGING){
+//this allows remote control of logging
+if (process.env.LOGGING) {
   delete config.logging;
 }
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db', config);
+const conn = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost/acme_db',
+  config
+);
 
+//models---------------------------------------------------
 const User = conn.define('user', {
   username: STRING,
-  password: STRING
+  password: STRING,
 });
 
-User.addHook('beforeSave', async(user)=> {
-  if(user.changed('password')){
+const Note = conn.define('note', {
+  text: STRING,
+});
+
+Note.belongsTo(User);
+User.hasMany(Note);
+
+//hooks------------------------------------------------
+User.addHook('beforeSave', async (user) => {
+  if (user.changed('password')) {
     const hashed = await bcrypt.hash(user.password, 3);
     user.password = hashed;
   }
 });
 
-User.byToken = async(token)=> {
+//class methods---------------------------------------
+User.byToken = async (token) => {
   try {
     const payload = await jwt.verify(token, process.env.JWT);
     const user = await User.findByPk(payload.id, {
       attributes: {
-        exclude: ['password']
-      }
+        exclude: ['password'],
+      },
     });
-    if(user){
+    if (user) {
       return user;
     }
     const error = Error('bad credentials');
     error.status = 401;
     throw error;
-  }
-  catch(ex){
+  } catch (ex) {
     const error = Error('bad credentials');
     error.status = 401;
     throw error;
   }
 };
 
-User.authenticate = async({ username, password })=> {
+User.authenticate = async ({ username, password }) => {
+  //authenticate returns a jwt, called in api
+  //if 2 servers use the same secret key, same jwt works in both
+  //jwt allows auth token to be stored in client
   const user = await User.findOne({
     where: {
-      username
-    }
-  });
-  if(user && await bcrypt.compare(password, user.password) ){
-    return jwt.sign({ id: user.id}, process.env.JWT); 
+      username,
+    },
+  }); //bcrypt next subject
+  if (user && (await bcrypt.compare(password, user.password))) {
+    //jwt.sign returns token
+    return jwt.sign({ id: user.id }, process.env.JWT);
   }
   const error = Error('bad credentials!!!!!!');
   error.status = 401;
   throw error;
 };
 
-const syncAndSeed = async()=> {
+//seed db----------------------------------------
+const syncAndSeed = async () => {
   await conn.sync({ force: true });
   const credentials = [
-    { username: 'lucy', password: 'lucy_pw'},
-    { username: 'moe', password: 'moe_pw'},
-    { username: 'larry', password: 'larry_pw'}
+    { username: 'lucy', password: 'lucy_pw' },
+    { username: 'moe', password: 'moe_pw' },
+    { username: 'larry', password: 'larry_pw' },
   ];
   const [lucy, moe, larry] = await Promise.all(
-    credentials.map( credential => User.create(credential))
+    credentials.map((credential) => User.create(credential))
   );
+  const notes = [
+    { text: 'buy bread', userId: lucy.id },
+    { text: 'text ricky', userId: lucy.id },
+    { text: 'email moe', userId: larry.id },
+  ];
+  await Promise.all(notes.map((note) => Note.create(note)));
   return {
     users: {
       lucy,
       moe,
-      larry
-    }
+      larry,
+    },
   };
 };
 
 module.exports = {
   syncAndSeed,
   models: {
-    User
-  }
+    User,
+  },
 };
