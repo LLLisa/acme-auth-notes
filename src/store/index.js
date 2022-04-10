152154e6f95a5b20ce75072184d4d2cd2f1dbed3
Lisa Knox <lisaknox83@gmail.com>
2022-04-10 16:38:18 -0400

    thunk good i think

--- src/store/index.js
+++ src/store/index.js
@@ -3,51 +3,70 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import axios from 'axios';
 
-const notes = (state = [], action)=> {
+const notes = (state = [], action) => {
+  if (action.type === 'SET_NOTES') {
+    return action.notes;
+  }
   return state;
 };
 
-const auth = (state = {}, action)=> {
-  if(action.type === 'SET_AUTH'){
+const auth = (state = {}, action) => {
+  if (action.type === 'SET_AUTH') {
     return action.auth;
   }
   return state;
 };
 
-const logout = ()=> {
+const logout = () => {
   window.localStorage.removeItem('token');
   return {
     type: 'SET_AUTH',
-    auth: {}
+    auth: {},
   };
 };
 
-const signIn = (credentials)=> {
-  return async(dispatch)=> {
+const signIn = (credentials) => {
+  return async (dispatch) => {
     let response = await axios.post('/api/auth', credentials);
     const { token } = response.data;
     window.localStorage.setItem('token', token);
     return dispatch(attemptLogin());
-  }
+  };
 };
-const attemptLogin = ()=> {
-  return async(dispatch)=> {
+const attemptLogin = () => {
+  return async (dispatch) => {
     const token = window.localStorage.getItem('token');
-    if(token){
+    if (token) {
       const response = await axios.get('/api/auth', {
         headers: {
-          authorization: token
-        }
+          authorization: token,
+        },
       });
       dispatch({ type: 'SET_AUTH', auth: response.data });
     }
-  }
-}
+  };
+};
+
+const getNotes = () => {
+  return async (dispatch) => {
+    const token = window.localStorage.getItem('token');
+    if (token) {
+      const notes = (
+        await axios.get('/api/notes', {
+          headers: {
+            authorization: token,
+          },
+        })
+      ).data;
+      dispatch({ type: 'SET_NOTES', notes });
+    }
+  };
+};
 
 const store = createStore(
   combineReducers({
     auth,
-    notes
+    notes,
   }),
   applyMiddleware(thunk, logger)
 );
